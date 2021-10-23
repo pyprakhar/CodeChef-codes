ec39b90b25322e1c612e248570ea9c49e675deef
duplixx <90516956+duplixx@users.noreply.github.com>
2021-10-23 22:40:21 +0530

    Update duplixx-ques2-HACK2021.c

--- duplixx-ques2-HACK2021.c
+++ duplixx-ques2-HACK2021.c
@@ -1,29 +1,32 @@
+#include <stdio.h>
 int main()
 {
-    int a, b, c;
-    printf("3");
-
-
-    scanf("%d%d%d", &a, &b, &c);
-    if(a>b && a>c)
-    {
+    int a, b, c,t,i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++){
+     scanf("%d%d%d", &a, &b, &c);
+     if(a>b && a>c)
+     {
         if(b>c)
-            printf("%d", b);
+            printf("%d\n", b);
         else
-            printf("%d", c);
-    }
-    else if(b>c && b>a)
-    {
+            printf("%d\n", c);
+     }
+     else if(b>c && b>a)
+     {
         if(c>a)
-            printf("%d", c);
+            printf("%d\n", c);
         else
-            printf("%d", a);
-    }
-    else if(a>b)
-            printf("%d", a);
+            printf("%d\n", a);
+     }
+     else if(a>b)
+            printf("%d\n", a);
         else
-            printf("%d", b);
+            printf("%d\n", b);
+     }
     return 0;
+
 }
+
 
 
