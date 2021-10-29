e890039af6eb83fc07e3d9ed63cc33e03e981796
SHEKHAR PATEL <90516956+duplixx@users.noreply.github.com>
2021-10-30 02:04:49 +0530

    Create duplixx-ques4-HACK21.cpp

---
+++ ANSWER _UPLOADS/duplixx-ques4-HACK21.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+bool balance(string expr) 
+{ 
+    stack<char> s; 
+    char x; 
+ 
+    for (int i=0; i<expr.length(); i++) 
+    { 
+        if (expr[i]=='('||expr[i]=='['||expr[i]=='{') 
+        { 
+            s.push(expr[i]); 
+            continue; 
+        } 
+        if (s.empty()) 
+           return false; 
+  
+        switch (expr[i]) 
+        { 
+        case ')': 
+            x = s.top(); 
+            s.pop(); 
+            if (x=='{' || x=='[') 
+                return false; 
+            break; 
+  
+        case '}': 
+  
+            x = s.top(); 
+            s.pop(); 
+            if (x=='(' || x=='[') 
+                return false; 
+            break; 
+  
+        case ']': 
+            x = s.top(); 
+            s.pop(); 
+            if (x =='(' || x == '{') 
+                return false; 
+            break; 
+        } 
+    } 
+    return (s.empty()); 
+} 
+int main() 
+{
+  string expr;
+  cout<<"Enter a string : ";
+  cin>>expr;
+    if (balance(expr)) 
+        cout << "Balanced"; 
+    else
+        cout << "Not Balanced"; 
+    return 0; 
+}
